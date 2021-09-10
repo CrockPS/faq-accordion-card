41f8aa957fa975e13d95dbae2ab8e26c27280a81
Cássio Ribeiro <crcassioribeiro@gmail.com>
2021-09-09 22:39:54 -0300

    primeiro envio

---
+++ main.js
@@ -0,0 +1,30 @@
+function rotate1(){
+    let arrow = document.getElementById('arrow1');
+    let p = document.querySelector('#p1');
+    arrow.classList.toggle('arrow2');
+    p.classList.toggle('p-transform')
+}
+function rotate2(){
+    let arrow = document.getElementById('arrow2');
+    let p = document.querySelector('#p2');
+    arrow.classList.toggle('arrow2');
+    p.classList.toggle('p-transform')
+}
+function rotate3(){
+    let arrow = document.getElementById('arrow3');
+    let p = document.querySelector('#p3');
+    arrow.classList.toggle('arrow2');
+    p.classList.toggle('p-transform')
+}
+function rotate4(){
+    let arrow = document.getElementById('arrow4');
+    let p = document.querySelector('#p4');
+    arrow.classList.toggle('arrow2');
+    p.classList.toggle('p-transform')
+}
+function rotate5(){
+    let arrow = document.getElementById('arrow5');
+    let p = document.querySelector('#p5');
+    arrow.classList.toggle('arrow2');
+    p.classList.toggle('p-transform')
+}
